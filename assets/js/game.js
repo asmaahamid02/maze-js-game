f6256b906f24bbf80cbd49d528b86a2278ad20ee
asmaahamid02 <asmaahamid002@gmail.com>
2022-09-03 15:59:43 +0300

    add score to the losing case and change the header text

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,26 @@
 window.addEventListener('load', () => {
-  const boundaries = document.querySelectorAll('.boundary');
+  var score = 0
+  const container = document.querySelector('#game')
+  const boundaries = document.querySelectorAll('.boundary')
+  const defaultHeaderText = document.querySelector('#status').innerText
+  console.log(defaultHeaderText)
+  const status = document.querySelector('#status')
+  const scoreContainer = document.querySelector('.example')
 
-  boundaries.forEach((boundary) => {
-    boundary.addEventListener('mouseover', () => {
+  scoreContainer.innerHTML = score
+  scoreContainer.style.color = '#000'
+  scoreContainer.style.textAlign = 'center'
+
+  container.addEventListener('mouseover', (e) => {
+    if (e.target.matches('.boundary')) {
       boundaries.forEach((boundary) => {
-        boundary.style.backgroundColor = '#ea0505';
-      });
-    });
-  });
-});
+        boundary.style.backgroundColor = '#ea0505'
+      })
+      status.innerText = 'You Lost!'
+      status.style.color = '#ea0505'
+      score -= 10
+      scoreContainer.innerHTML = score
+      scoreContainer.style.color = '#fff'
+    }
+  })
+})
